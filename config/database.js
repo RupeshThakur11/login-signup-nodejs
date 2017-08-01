862e68b63aacae1d2bedebac278e40c0c694eaad
Rupesh Thakur <rkdaredevil@users.noreply.github.com>
2017-08-01 20:13:41 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost:27017/myproject' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	//'url' : 'mongodb://localhost:27017/myproject' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://pots:rupesh123@@ds127993.mlab.com:27993/pots-crud'
 
 };
